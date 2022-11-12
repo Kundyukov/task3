3ee6240bd789270e25305720e899935a66c548c5
Kundyukov <116679446+Kundyukov@users.noreply.github.com>
2022-11-12 23:56:13 +0300

    Add files via upload

--- MyForm.h
+++ MyForm.h
@@ -44,12 +44,15 @@ namespace task3 {
 	private: System::Windows::Forms::Label^ label3;
 
 	private: System::Windows::Forms::TextBox^ textbox1;
-	private: System::Windows::Forms::Label^ label4;
+
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::DataVisualization::Charting::Chart^ chart1;
 	private: System::Windows::Forms::NumericUpDown^ numericUpDown1;
 	private: System::Windows::Forms::NumericUpDown^ numericUpDown2;
 	private: System::Windows::Forms::NumericUpDown^ numericUpDown3;
+
+	private: System::Windows::Forms::GroupBox^ groupBox1;
+	private: System::Windows::Forms::GroupBox^ groupBox2;
 
 
 	private:
@@ -72,22 +75,27 @@ namespace task3 {
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->textbox1 = (gcnew System::Windows::Forms::TextBox());
-			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->chart1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
 			this->numericUpDown1 = (gcnew System::Windows::Forms::NumericUpDown());
 			this->numericUpDown2 = (gcnew System::Windows::Forms::NumericUpDown());
 			this->numericUpDown3 = (gcnew System::Windows::Forms::NumericUpDown());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->groupBox2 = (gcnew System::Windows::Forms::GroupBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown2))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown3))->BeginInit();
+			this->groupBox1->SuspendLayout();
+			this->groupBox2->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(12, 34);
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 7.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(6, 19);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(67, 16);
 			this->label1->TabIndex = 1;
@@ -96,7 +104,9 @@ namespace task3 {
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(12, 78);
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 7.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(6, 63);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(77, 16);
 			this->label2->TabIndex = 2;
@@ -105,7 +115,9 @@ namespace task3 {
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(12, 122);
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 7.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label3->Location = System::Drawing::Point(6, 107);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(38, 16);
 			this->label3->TabIndex = 4;
@@ -113,39 +125,35 @@ namespace task3 {
 			// 
 			// textbox1
 			// 
+			this->textbox1->BackColor = System::Drawing::SystemColors::Window;
 			this->textbox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 7.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->textbox1->Location = System::Drawing::Point(127, 53);
+			this->textbox1->Location = System::Drawing::Point(7, 15);
 			this->textbox1->Multiline = true;
 			this->textbox1->Name = L"textbox1";
 			this->textbox1->ReadOnly = true;
-			this->textbox1->ScrollBars = System::Windows::Forms::ScrollBars::Vertical;
+			this->textbox1->ScrollBars = System::Windows::Forms::ScrollBars::Both;
 			this->textbox1->Size = System::Drawing::Size(259, 298);
 			this->textbox1->TabIndex = 6;
 			this->textbox1->TabStop = false;
 			// 
-			// label4
-			// 
-			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(158, 34);
-			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(48, 16);
-			this->label4->TabIndex = 7;
-			this->label4->Text = L"Result:";
-			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(15, 190);
+			this->button1->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 7.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->Location = System::Drawing::Point(9, 272);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(91, 41);
-			this->button1->TabIndex = 8;
+			this->button1->Size = System::Drawing::Size(94, 41);
+			this->button1->TabIndex = 12;
 			this->button1->Text = L"Solve";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// chart1
 			// 
-			this->chart1->BackColor = System::Drawing::Color::WhiteSmoke;
+			this->chart1->BackColor = System::Drawing::Color::Transparent;
+			this->chart1->BorderlineColor = System::Drawing::Color::Transparent;
 			chartArea3->AxisX->IntervalAutoMode = System::Windows::Forms::DataVisualization::Charting::IntervalAutoMode::VariableCount;
 			chartArea3->AxisX->IsStartedFromZero = false;
 			chartArea3->AxisX2->IntervalAutoMode = System::Windows::Forms::DataVisualization::Charting::IntervalAutoMode::VariableCount;
@@ -154,12 +162,13 @@ namespace task3 {
 			chartArea3->AxisY->IsStartedFromZero = false;
 			chartArea3->AxisY2->IntervalAutoMode = System::Windows::Forms::DataVisualization::Charting::IntervalAutoMode::VariableCount;
 			chartArea3->AxisY2->IsStartedFromZero = false;
+			chartArea3->BackColor = System::Drawing::Color::Transparent;
 			chartArea3->Name = L"ChartArea1";
 			this->chart1->ChartAreas->Add(chartArea3);
 			legend3->Enabled = false;
 			legend3->Name = L"Legend1";
 			this->chart1->Legends->Add(legend3);
-			this->chart1->Location = System::Drawing::Point(392, 53);
+			this->chart1->Location = System::Drawing::Point(405, 34);
 			this->chart1->Name = L"chart1";
 			this->chart1->Palette = System::Windows::Forms::DataVisualization::Charting::ChartColorPalette::Berry;
 			series3->BorderWidth = 3;
@@ -168,53 +177,91 @@ namespace task3 {
 			series3->Legend = L"Legend1";
 			series3->Name = L"Series1";
 			this->chart1->Series->Add(series3);
-			this->chart1->Size = System::Drawing::Size(459, 298);
+			this->chart1->Size = System::Drawing::Size(459, 309);
 			this->chart1->TabIndex = 9;
 			this->chart1->TabStop = false;
 			this->chart1->Text = L"chart1";
 			// 
 			// numericUpDown1
 			// 
-			this->numericUpDown1->Location = System::Drawing::Point(15, 53);
+			this->numericUpDown1->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->numericUpDown1->DecimalPlaces = 2;
+			this->numericUpDown1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 7.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->numericUpDown1->Location = System::Drawing::Point(9, 38);
 			this->numericUpDown1->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 100, 0, 0, System::Int32::MinValue });
 			this->numericUpDown1->Name = L"numericUpDown1";
 			this->numericUpDown1->Size = System::Drawing::Size(94, 22);
-			this->numericUpDown1->TabIndex = 10;
+			this->numericUpDown1->TabIndex = 8;
 			this->numericUpDown1->ThousandsSeparator = true;
 			// 
 			// numericUpDown2
 			// 
-			this->numericUpDown2->Location = System::Drawing::Point(15, 97);
+			this->numericUpDown2->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->numericUpDown2->DecimalPlaces = 2;
+			this->numericUpDown2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 7.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->numericUpDown2->Location = System::Drawing::Point(9, 82);
 			this->numericUpDown2->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 100, 0, 0, System::Int32::MinValue });
 			this->numericUpDown2->Name = L"numericUpDown2";
 			this->numericUpDown2->Size = System::Drawing::Size(94, 22);
-			this->numericUpDown2->TabIndex = 11;
+			this->numericUpDown2->TabIndex = 9;
 			this->numericUpDown2->ThousandsSeparator = true;
 			// 
 			// numericUpDown3
 			// 
-			this->numericUpDown3->Location = System::Drawing::Point(15, 141);
+			this->numericUpDown3->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->numericUpDown3->DecimalPlaces = 2;
+			this->numericUpDown3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 7.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->numericUpDown3->Location = System::Drawing::Point(9, 126);
 			this->numericUpDown3->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 100, 0, 0, System::Int32::MinValue });
 			this->numericUpDown3->Name = L"numericUpDown3";
 			this->numericUpDown3->Size = System::Drawing::Size(94, 22);
-			this->numericUpDown3->TabIndex = 12;
+			this->numericUpDown3->TabIndex = 10;
 			this->numericUpDown3->ThousandsSeparator = true;
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Controls->Add(this->numericUpDown3);
+			this->groupBox1->Controls->Add(this->numericUpDown2);
+			this->groupBox1->Controls->Add(this->button1);
+			this->groupBox1->Controls->Add(this->numericUpDown1);
+			this->groupBox1->Controls->Add(this->label3);
+			this->groupBox1->Controls->Add(this->label2);
+			this->groupBox1->Controls->Add(this->label1);
+			this->groupBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 7.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->groupBox1->Location = System::Drawing::Point(12, 19);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(109, 324);
+			this->groupBox1->TabIndex = 7;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"Values";
+			this->groupBox1->Enter += gcnew System::EventHandler(this, &MyForm::groupBox1_Enter);
+			// 
+			// groupBox2
+			// 
+			this->groupBox2->Controls->Add(this->textbox1);
+			this->groupBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 7.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->groupBox2->Location = System::Drawing::Point(127, 19);
+			this->groupBox2->Name = L"groupBox2";
+			this->groupBox2->Size = System::Drawing::Size(272, 324);
+			this->groupBox2->TabIndex = 15;
+			this->groupBox2->TabStop = false;
+			this->groupBox2->Text = L"Result";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(877, 430);
-			this->Controls->Add(this->numericUpDown3);
-			this->Controls->Add(this->numericUpDown2);
-			this->Controls->Add(this->numericUpDown1);
+			this->ClientSize = System::Drawing::Size(877, 418);
+			this->Controls->Add(this->groupBox2);
+			this->Controls->Add(this->groupBox1);
 			this->Controls->Add(this->chart1);
-			this->Controls->Add(this->button1);
-			this->Controls->Add(this->label4);
-			this->Controls->Add(this->textbox1);
-			this->Controls->Add(this->label3);
-			this->Controls->Add(this->label2);
-			this->Controls->Add(this->label1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->MaximizeBox = false;
 			this->Name = L"MyForm";
 			this->Text = L"Tabulation";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
@@ -222,8 +269,11 @@ namespace task3 {
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown2))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown3))->EndInit();
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox1->PerformLayout();
+			this->groupBox2->ResumeLayout(false);
+			this->groupBox2->PerformLayout();
 			this->ResumeLayout(false);
-			this->PerformLayout();
 
 		}
 #pragma endregion
@@ -235,12 +285,34 @@ namespace task3 {
 		leftval = Convert::ToDouble(numericUpDown1->Text);
 		rightval = Convert::ToDouble(numericUpDown2->Text);
 		step = Convert::ToDouble(numericUpDown3->Text);
+		this->chart1->Series[0]->Points->Clear();
+		this->textbox1->Clear();
 		for (x = leftval; x <= rightval; x += step) {
 			textbox1->AppendText("x = " + Convert::ToString(x) + "\ty = " + Convert::ToString(y = f.tabulation(leftval, rightval, step, x)) + "\r\n");
-			//chart1->Series["Series1"]->Points->AddXY(x, y = f.tabulation(leftval, rightval, step, x));
+
+
+			try {
+				if (y < INFINITY) {
+					chart1->Series["Series1"]->Points->AddXY(x, y = f.tabulation(leftval, rightval, step, x));
+				}
+				if (y >= INFINITY) {
+					throw - y;
+				}
+			}
+			catch (double y) {
+				textbox1->AppendText("[ INFINITE DISCONTINUITY ]"+"\r\n");
+			}
+
+
 		}
 	}
 
+private: System::Void groupBox1_Enter(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label4_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
 };
 
 }
